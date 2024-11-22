fb5e8de484638e20924cb0f68f8e5cbb85724ecd
Gheris <103877241+Gheris-579@users.noreply.github.com>
2024-11-22 23:04:30 +0100

    Add files via upload

--- Form/js/script.js
+++ Form/js/script.js
@@ -6,12 +6,12 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
 
     // Here ye can add yer logic to check the credentials
     if (username === 'admin@123gmail.com' && password === 'admin') {
-        document.getElementById('message').textContent = 'Accesso riuscito, benvenuto!';
+        document.getElementById('message').textContent = 'Login successful, welcome!';
         document.getElementById('message').style.color = "green";
 
     } 	
 	else {
-        document.getElementById('message').textContent = 'Nome utente o password errati!';
+        document.getElementById('message').textContent = 'Incorrect username or password!';
         document.getElementById('message').style.color = "red";
     }
 });
